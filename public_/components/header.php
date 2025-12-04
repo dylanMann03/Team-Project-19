4f71d8a97a88f09110198eb0209d83770dd55325
alitoo007 <alitobonjour@gmail.com>
2025-12-04 23:57:26 +0000

    Add favicon link to header.php

--- public_/components/header.php
+++ public_/components/header.php
@@ -7,6 +7,7 @@ $current = basename($_SERVER['PHP_SELF']);
     <meta charset="UTF-8">
     <title>Bakes & Cakes | Your home for all your bakes and cakes</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="css/styles.css">
  </head>
 
